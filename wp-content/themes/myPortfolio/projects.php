da759d6c265ece46a705f075547be4aa3dd0a7a6
飯村拓也 <keepsrh02@gmail.com>
2020-09-05 09:44:11 +0900

    [add] wp上に投稿用メディアを追加

---
+++ wp-content/themes/myPortfolio/projects.php
@@ -0,0 +1,59 @@
+<?php
+/*
+Template Name: projects
+*/
+?>
+
+<?php get_header(); ?>
+      <!-- ---↓main--------------------------------------------------------------------- -->
+      <main class="projectsPage common mask">
+        <div class="inner">
+          <h1 class="pageHeading"><?php the_title(); ?></h1>
+          <div class="spImageWrap">
+            <div class="spImage"></div>
+          </div>
+          <div class="contentsWrap">
+            <p class="scrollIcon"><span></span></p>
+            <ul class="projectList">
+              <li class="item">
+                <p class="imageWrap">
+                  <img src="./assets/img/mockup_somu-min.png" alt="">
+                </p>
+                <div class="overview">
+                  <h2 class="heading">官公庁系サイトのリプレース案件</h2>
+                  <p class="text">
+                    テキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああ
+                  </p>
+                  <p class="next"><a class="btn" href="./project.html">read more</a></p>
+                </div>
+              </li>
+              <li class="item">
+                <p class="imageWrap">
+                  <img src="./assets/img/mockup_es-min.png" alt="">
+                </p>
+                <div class="overview">
+                  <h2 class="heading">コーポレートサイト制作案件</h2>
+                  <p class="text">
+                    テキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああ
+                  </p>
+                  <p class="next"><a class="btn" href="./project.html">read more</a></p>
+                </div>
+              </li>
+              <li class="item">
+                <p class="imageWrap">
+                  <img src="./assets/img/mockup_somu-min.png" alt="">
+                </p>
+                <div class="overview">
+                  <h2 class="heading">官公庁系サイトのリプレース案件</h2>
+                  <p class="text">
+                    テキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああテキストどうしようかなあああああああうわあああああ
+                  </p>
+                  <p class="next"><a class="btn" href="./project.html">read more</a></p>
+                </div>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </main>
+      <!-- ./main -->
+<?php get_footer(); ?>
